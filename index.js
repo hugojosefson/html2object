7a328f05834a40fa3580fba2d42726f9dc8318e9
Hugo Josefson <hugo@josefson.org>
2013-04-27 18:10:38 +0200

    Refactor.

--- index.js
+++ index.js
@@ -7,19 +7,20 @@ module.exports = html2object;
 
 function html2object(html) {
   var nodes = domify(html);
+  return nodes2object(nodes);
+}
+
+function nodes2object(nodes) {
   var result = {};
-  _.each(nodes, function(node){
+  _.each(nodes, function (node) {
     possiblyAddNodeToResult(result, node);
   });
   return result;
 }
 
 function possiblyAddNodeToResult(result, node) {
-  if (node.id) {
-    result[node.id] = node;
-  } else if (node.name) {
-    result[node.name] = node;
-  } else if (node.class) {
-    result[node.class] = node;
+  var key = node.id || node.name || node.class;
+  if (key) {
+    result[key] = node;
   }
 }
